e46c02baac7434ba8c1e8a5b25b0de2a141226a0
洋洋兮若江河 <@DESKTOP-87GENQ4>
2025-02-02 11:33:53 +0800

    添加项目文件。

---
+++ SubModule.cs
@@ -0,0 +1,27 @@
+﻿using TaleWorlds.MountAndBlade;
+
+
+namespace FirstMod
+{
+    public class SubModule : MBSubModuleBase
+    {
+		
+        protected override void OnSubModuleLoad()
+        {
+            base.OnSubModuleLoad();
+
+        }
+
+        protected override void OnSubModuleUnloaded()
+        {
+            base.OnSubModuleUnloaded();
+
+        }
+
+        protected override void OnBeforeInitialModuleScreenSetAsRoot()
+        {
+            base.OnBeforeInitialModuleScreenSetAsRoot();
+
+        }
+    }
+}
